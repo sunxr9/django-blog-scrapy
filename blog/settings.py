5dce920dc61026893c70470f8ab084d1929da576
sunxr9 <sunxiaoran9@163.com>
2018-05-19 17:43:56 +0800

    配置redis

--- blog/settings.py
+++ blog/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = ['*']
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/0",
+        "LOCATION": "redis://:sunxr@127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
